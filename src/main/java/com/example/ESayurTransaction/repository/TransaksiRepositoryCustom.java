aefb4435d3b080c97d44444416fb4932ff327698
Leo Masta <leomastakusuma@gmail.com>
2020-12-26 21:05:41 +0700

    update

--- src/main/java/com/example/ESayurTransaction/repository/TransaksiRepositoryCustom.java
+++ src/main/java/com/example/ESayurTransaction/repository/TransaksiRepositoryCustom.java
@@ -1,6 +1,5 @@
 package com.example.ESayurTransaction.repository;
 
-import com.example.ESayurTransaction.data.Order;
 import com.example.ESayurTransaction.model.Transaksi;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,8 +10,6 @@ public interface TransaksiRepositoryCustom {
     @Query(value="select *  from transaksi t where t.id_user =:idUser", nativeQuery=true)
     List<Transaksi> getByUser(@Param("idUser") Long idUser);
 
-    @Query(value="select  id_user,count(id_user) totalBelanja,sum(price) totalPrice FROM transaksi where id_grobak = 2 GROUP BY id_user",nativeQuery = true)
-    List<Order> listOrderMe (@Param("idGrobak") Long idGrobak);
 
     @Query(value="select *  from transaksi t where t.id_transaksi =:idTransaki", nativeQuery=true)
     Transaksi  getById(@Param("idTransaki") Long idTransaki);
--- src/main/java/com/example/ESayurTransaction/repository/TransaksiRepositoryImpl.java
+++ src/main/java/com/example/ESayurTransaction/repository/TransaksiRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.example.ESayurTransaction.repository;
 
-import com.example.ESayurTransaction.data.Order;
 import com.example.ESayurTransaction.model.Transaksi;
 
 import java.util.List;
@@ -8,11 +7,6 @@ import java.util.List;
 public class TransaksiRepositoryImpl implements  TransaksiRepositoryCustom{
     @Override
     public List<Transaksi> getByUser(Long idUser) {
-        return null;
-    }
-
-    @Override
-    public List<Order> listOrderMe(Long idGrobak) {
         return null;
     }
 
